3b65763089b28049e6f94dce550f7b203d66b270
Mostafa Mahmoud Abd El-Aleem <mostafamahmoudabdelalim@hotmail.com>
2020-03-20 00:49:40 +0200

    Express API & MongoDB

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://root:r123456@cluster0-cq7l7.azure.mongodb.net/mongoose'
+}
--- server.js
+++ server.js
@@ -1,7 +1,25 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const items = require('./routes/api/items')
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
+
+// uses body-parser middelware
+app.use(bodyParser.json());
+
+//DB configs
+const db = require('./config/keys').mongoURI;
+
+//connect DB
+mongoose
+    .connect(db)
+    .then(() => console.log('DB Connected .....'))
+    .catch(err => console.log(err))
+
+app.use('/api/items/', items);
 
 app.listen(port, () => {
     console.log(`Server Started at Port ${port}`);
